a29d5e8c55ed8fc5e24833601ccee377ffa6335f
Emmanuel <132662856+Emmanuel2-uni@users.noreply.github.com>
2023-10-11 17:50:47 +0800

    Create Sedan.java

---
+++ Exercise_4/Sedan.java
@@ -0,0 +1,9 @@
+public class Sedan extends Car {
+    public Sedan(String name){
+        super(name);
+    }
+
+        public void displaySedan(){
+            System.out.printf("\n%s is a Car of the type Sedan.", name);
+        }
+}
