2afccd0523c78f200f34ecb4801a2f863336757c
Emmanuel <132662856+Emmanuel2-uni@users.noreply.github.com>
2023-10-11 17:50:16 +0800

    Create App.java

---
+++ Exercise_4/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Take a variable String (name) for use in the three methods.
+        Sedan vios = new Sedan("Toyota Vios");
+        
+        vios.displayVehicle();
+        vios.displaySedan();
+        vios.displayCar();
+      }
+  }
