7bbbcc83d2e6af04db6414918d9159c19eb86561
Emmanuel <132662856+Emmanuel2-uni@users.noreply.github.com>
2023-10-11 17:04:51 +0800

    !! CREATE A NEW JAVA FILE FOR EACH CLASS !!

--- Exercise3.java
+++ Exercise3.java
@@ -1,7 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        // Take a variable String (name) for use in the three methods.
         Sedan vios = new Sedan("Toyota Vios");
-
+        
         vios.displayVehicle();
         vios.displaySedan();
         vios.displayCar();
