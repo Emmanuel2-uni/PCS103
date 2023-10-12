89c601cd93cceaed34ddb460dd762ad981fb2d36
Emmanuel <132662856+Emmanuel2-uni@users.noreply.github.com>
2023-10-12 17:22:19 +0800

    Create Person.java

---
+++ Midterm_Exam/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    String firstname;
+    String lastname;
+
+    public Person(String firstname, String lastname){
+        this.firstname = firstname;
+        this.lastname = lastname;
+
+    }
+
+}
