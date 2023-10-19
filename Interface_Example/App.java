93a1b49dae2f85eafcfeb816e5689e0a0ac676f8
Emmanuel <132662856+Emmanuel2-uni@users.noreply.github.com>
2023-10-19 17:55:14 +0800

    Create App.java

---
+++ Interface_Example/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        Sparrow Jack = new Sparrow(2, 2, "Male");
+        Jack.fly();
+
+        Chicken Chick1 = new Chicken(1, 1, "Female");
+        Chick1.fly();
+    }
+}
