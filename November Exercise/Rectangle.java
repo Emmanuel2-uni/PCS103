7a276ae69c31976f8472940c4b05180c0e08be1f
Emmanuel <132662856+Emmanuel2-uni@users.noreply.github.com>
2023-11-27 19:52:04 +0800

    Create Rectangle.java

---
+++ November Exercise/Rectangle.java
@@ -0,0 +1,9 @@
+public class Rectangle extends Shape implements Calc{
+
+    public double calculateArea(double width, double height){
+        double area = width * height;
+        return area;
+    }
+
+
+}
